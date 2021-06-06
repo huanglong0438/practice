122fb772cbb812f406f0b4b9a8dfd5f532c0cb31
huanglong0438 <443795530@qq.com>
2021-06-06 20:49:12 +0800

    find intersection node

---
+++ src/main/java/jianzhi_offer/common/ListNode.java
@@ -0,0 +1,20 @@
+package jianzhi_offer.common;
+
+/**
+ * ListNode
+ *
+ * @title ListNode
+ * @Description
+ * @Author donglongcheng01
+ * @Date 2021/2/26
+ **/
+public class ListNode {
+
+    public int key;
+    public ListNode next;
+
+    public ListNode(int key) {
+        this.key = key;
+    }
+
+}
